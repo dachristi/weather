d541e3847be7dcf69919a07f77b66bd85a114288
dc <dc108349@gmail.com>
2020-01-02 18:41:34 -0800

    Updated condition to handle one or more weather histories given in a file.

--- weather_parse.py
+++ weather_parse.py
@@ -20,41 +20,40 @@ def main():
 
 def parse_weather(file_path):
     print("Parsing...%s" % file_path.name)
-    #data = read_weather_data(data_file)
     data = json.loads(file_path.read_text())
     try:
-        data_list = data['features']
+        data_list = data['features']  # multiple weather histories were called
     except:
-        data_dict = data
-    #for data_dict in data_list:
-    properties = data_dict['properties']
-    station_id = properties['station'].split('/')[-1]
-    text_description = properties['textDescription'][:50]
-    timestamp = re.sub(r'T', ' ', properties['timestamp'].split('+')[0])
-    temperature = properties['temperature']['value']
-    temperature_unit = properties['temperature']['unitCode'].split(':')[-1]
-    dewpoint = properties['dewpoint']['value']
-    dewpoint_unit = properties['dewpoint']['unitCode'].split(':')[-1]
-    wind_direction = properties['windDirection']['value']
-    wind_direction_unit = properties['windDirection']['unitCode'].split(':')[-1]
-    wind_speed = properties['windSpeed']['value']
-    wind_speed_unit = properties['windSpeed']['unitCode'].split(':')[-1]
-    wind_gust = properties['windGust']['value']
-    wind_gust_unit = properties['windGust']['unitCode'].split(':')[-1]
-    barometric_pressure = properties['barometricPressure']['value']
-    barometric_pressure_unit = properties['barometricPressure']['unitCode'].split(':')[-1]
-    sea_level_pressure = properties['seaLevelPressure']['value']
-    sea_level_pressure_unit = properties['seaLevelPressure']['unitCode'].split(':')[-1]
-    visibility = properties['visibility']['value']
-    visibility_unit = properties['visibility']['unitCode'].split(':')[-1]
-    precipitation_last_hour = properties['precipitationLastHour']['value']
-    precipitation_last_hour_unit = properties['precipitationLastHour']['unitCode'].split(':')[-1]
-    relative_humidity = properties['relativeHumidity']['value']
-    relative_humidity_unit = properties['relativeHumidity']['unitCode'].split(':')[-1]
-    wind_chill = properties['windChill']['value']
-    wind_chill_unit = properties['windChill']['unitCode'].split(':')[-1]
-    heat_index = properties['heatIndex']['value']
-    heat_index_unit = handle_none(properties['heatIndex']['unitCode'])
+        data_list = [data]  # single weather history was called
+    for data_dict in data_list:
+        properties = data_dict['properties']
+        station_id = properties['station'].split('/')[-1]
+        text_description = properties['textDescription'][:50]
+        timestamp = re.sub(r'T', ' ', properties['timestamp'].split('+')[0])
+        temperature = properties['temperature']['value']
+        temperature_unit = properties['temperature']['unitCode'].split(':')[-1]
+        dewpoint = properties['dewpoint']['value']
+        dewpoint_unit = properties['dewpoint']['unitCode'].split(':')[-1]
+        wind_direction = properties['windDirection']['value']
+        wind_direction_unit = properties['windDirection']['unitCode'].split(':')[-1]
+        wind_speed = properties['windSpeed']['value']
+        wind_speed_unit = properties['windSpeed']['unitCode'].split(':')[-1]
+        wind_gust = properties['windGust']['value']
+        wind_gust_unit = properties['windGust']['unitCode'].split(':')[-1]
+        barometric_pressure = properties['barometricPressure']['value']
+        barometric_pressure_unit = properties['barometricPressure']['unitCode'].split(':')[-1]
+        sea_level_pressure = properties['seaLevelPressure']['value']
+        sea_level_pressure_unit = properties['seaLevelPressure']['unitCode'].split(':')[-1]
+        visibility = properties['visibility']['value']
+        visibility_unit = properties['visibility']['unitCode'].split(':')[-1]
+        precipitation_last_hour = properties['precipitationLastHour']['value']
+        precipitation_last_hour_unit = properties['precipitationLastHour']['unitCode'].split(':')[-1]
+        relative_humidity = properties['relativeHumidity']['value']
+        relative_humidity_unit = properties['relativeHumidity']['unitCode'].split(':')[-1]
+        wind_chill = properties['windChill']['value']
+        wind_chill_unit = properties['windChill']['unitCode'].split(':')[-1]
+        heat_index = properties['heatIndex']['value']
+        heat_index_unit = handle_none(properties['heatIndex']['unitCode'])
 
     conditions = (station_id,
                   timestamp,
