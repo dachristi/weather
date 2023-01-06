f06da1d6e481acfd2a1c7bf916a1dfb52aafb03d
dc <dc108349@gmail.com>
2023-01-05 16:25:39 -0800

    Added functions to add stations to table from file

--- stations.py
+++ stations.py
@@ -1,7 +1,7 @@
 #!/home/pc/Envs/weather/bin/python
 # -*- coding: utf-8 -*-
 
-
+import argparse
 import requests
 import json
 import re
@@ -13,7 +13,7 @@ from sql_fns import store_station_data
 from sql_fns import store_weather_data
 from sql_fns import query_station_ids
 
-class Stations(object):
+class Station(object):
     '''
     Given coordinates, retreive the list of stations and store them in a table.
     '''
@@ -36,7 +36,7 @@ class Stations(object):
             try:
                 station_longitude, station_latitude  = station_data['geometry']['coordinates']
                 station_id = station_data['properties']['stationIdentifier']
-                store_station_data(self.property_id, round(station_latitude, 6), round(station_longitude, 6), station_id)
+                store_station_data(self.property_id, round(station_latitude, 7), round(station_longitude, 7), station_id)
                 station_count += 1
             except:
                 print(station_data)
@@ -87,7 +87,55 @@ class WeatherApi(object):
         return None
 
 
+def initialize_stations_table():
+    station_file_path = 'station_data.json'
+    stations_data = read_station_data(station_file_path)
+    store_station_data(stations_data)
+
+
+def store_station_data(stations_data):
+    '''Store station data in the stations table.'''
+    data_list = []
+    for station in stations_data:
+        station_id = station['properties']['stationIdentifier']
+        coordinate_1 = station['geometry']['coordinates'][0]
+        coordinate_2 = station['geometry']['coordinates'][1]
+        elevation = station['properties']['elevation']['value']
+        elevation_units = station['properties']['elevation']['unitCode'].split(':')[1]
+        url = station['id']
+        tz = station['properties']['timeZone']
+        station_name = station['properties']['name']
+
+        data_list.append((station_id, coordinate_1, coordinate_2, elevation,
+                          elevation_units, url, tz, station_name))
+    sql_store_station_data(data_list)
+
+
+def read_station_data(file_path):
+    with open(file_path, 'r') as f:
+        data = json.load(f)
+    stations = data['features']
+    return stations
+
+
+def stations_api():
+    '''Pull the data and store it in a file'''
+
+    url = 'https://api.weather.gov/stations'
+    stations_text = requests.get(url)
+    stations_json = stations_text.json()
+    with open('station_data.json', 'w') as f:
+        json.dump(stations_json, f)
+    return None
+
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--init_stations', action='store_true', help='Initialize stations table')
+    args = parser.parse_args()
+    if args.init_stations:
+        initialize_stations_table()
+        print('Done')
+        return None
     station_ids = query_station_ids()
     station_count = 0
     for station_id in station_ids:
