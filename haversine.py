fdf271a4425ca151c910cb8205672cffc2d1b17a
David Christiansen <dchristiansen@gpgadvisers.com>
2023-01-16 16:57:21 -0800

    Renamed variables;

--- haversine.py
+++ haversine.py
@@ -2,7 +2,7 @@
 import math
 
 
-def distance2(lat1, lon1, lat2, lon2):
+def distance(lat1, lon1, lat2, lon2):
     lat1 = math.radians(lat1)
     lat2 = math.radians(lat2)
     delta_lat = (lat2 - lat1)
@@ -12,28 +12,24 @@ def distance2(lat1, lon1, lat2, lon2):
              math.sin(delta_lon / 2)**2 *
              math.cos(lat1) * math.cos(lat2))
     earth_radius = 6371
-    c = 2 * math.asin(math.sqrt(a))
-    d = earth_radius * c
+    central_angle = 2 * math.asin(math.sqrt(a))
+    d = earth_radius * central_angle
     return kilometers_to_miles(d)
 
 
 def distance(lat1, lon1, lat2, lon2):
-    '''
-    d=2*asin(sqrt((sin((lat1-lat2)/2))^2 +
-                     cos(lat1)*cos(lat2)*(sin((lon1-lon2)/2))^2))
-    Source: http://edwilliams.org/avform147.htm#Dist
-    Referenced from: https://www.nhc.noaa.gov/gccalc.shtml
-    '''
-
     lat1 = math.radians(lat1)
     lat2 = math.radians(lat2)
     lon1 = math.radians(lon1)
     lon2 = math.radians(lon2)
-    d = 2*math.asin(math.sqrt((math.sin((lat1-lat2)/2))**2 +
-                    math.cos(lat1)*math.cos(lat2)*(math.sin((lon1-lon2)/2))**2))
+    central_angle = 2 * math.asin(
+                            math.sqrt(
+                            (math.sin((lat1 - lat2)/2))**2 +
+                            math.cos(lat1) * math.cos(lat2) *
+                            (math.sin((lon1 - lon2)/2))**2))
     earth_radius = 6371  # kilometers
-    d = d * earth_radius
-    return kilometers_to_miles(d)
+    d = central_angle * earth_radius
+    return kilometers_to_miles(d)  # Unit=miles
 
 
 def kilometers_to_miles(km):
